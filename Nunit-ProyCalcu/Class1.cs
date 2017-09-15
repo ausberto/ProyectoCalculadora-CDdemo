2f09e9636313aec3329d403a611b0895e9664e0c
Unknown <ausberto7@hotmail.com>
2017-09-15 09:38:29 -0400

    Multiplicacion :v

---
+++ Nunit-ProyCalcu/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Nunit_ProyCalcu
+{
+  [TestFixture]
+  public class Class1
+  {
+    [Test]
+    public void Multi()
+    {
+      var objResp = new ProyectoCalculadora.Controllers.Operaciones();
+      Assert.AreEqual(0, objResp.Multiplicacion(1,0));
+    }
+
+    [Test]
+    public void Multi2()
+    {
+      var objResp = new ProyectoCalculadora.Controllers.Operaciones();
+      Assert.AreEqual(56, objResp.Multiplicacion(7,8));
+    }
+  }
+}
---
+++ ProyectoCalculadora/Controllers/Operaciones.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoCalculadora.Controllers
+{
+  public class Operaciones
+  {
+    public int Multiplicacion(int a, int b) {
+      return a * b;
+    }
+  }
+}
